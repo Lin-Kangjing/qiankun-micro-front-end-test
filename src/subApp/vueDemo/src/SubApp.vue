c9bb105ab3563c002bcf5893a2f5ef4f2f6d083a
Lin_Kangjing <linkangjing@foxmail.com>
2022-11-10 15:00:59 +0800

    删除SubApp.vue

---
+++ src/subApp/vueDemo/src/SubApp.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Description: 
+ * @FilePath: \micro-front-end\src\subApp\vueDemo\src\SubApp.vue
+ * @Date: 2022-11-10 11:18:40
+ * @LastEditors: Lin_kangjing
+ * @LastEditTime: 2022-11-10 11:18:50
+ * @author: Lin_kangjing
+-->
+<template>
+  <div id="subApp" class="subapp">
+
+  </div>
+</template>
+
+<script >
+export default {
+ components: {},
+ data() {
+   return {
+
+   }
+ },
+ created() {
+
+ },
+ mounted() {
+
+ },
+ methods: {
+
+ }
+}
+</script>
+
+<style scoped >
+</style>
